ef052bfc91bdd25375329ddf1acd435bc1fba55c
yvesls <yveslimasilva@gmail.com>
2023-06-28 19:50:43 -0300

    juntando a estrutura com o criar e listar tarefas ativas do Lukian

--- src/components/criar-tarefa/criarTarefa.js
+++ src/components/criar-tarefa/criarTarefa.js
@@ -1,3 +1,49 @@
 $(document).ready(function () {
     console.log("criar-tarefa");
+
+    $(".btn-add").click(function () {
+        addActivity();
+    });
+
+    function addActivity() {
+        var data = $("#data").val();
+        var horario = $("#horario").val();
+        var profissional = $("#profissional").val();
+        var descricao = $("#descricao").val();
+        var tempo = $("#tempo").val();
+
+        if (data && horario && profissional && descricao && tempo) {
+            var newRow = $("<tr>");
+            var cell1 = $("<td>").text(data);
+            var cell2 = $("<td>").text(horario);
+            var cell3 = $("<td>").text(profissional);
+            var cell4 = $("<td>").text(descricao);
+            var cell5 = $("<td>").text(tempo);
+            var cell6 = $("<td>").html('<button class="edit-button" onclick="editActivity(this)">Editar</button>');
+            var cell7 = $("<td>").html('<button class="forward-button" onclick="openDialog(this)">Encaminhar</button>');
+
+            newRow.append(cell1, cell2, cell3, cell4, cell5, cell6, cell7);
+            $("#activity-table tbody").append(newRow);
+
+            // Limpar os campos do formulário
+            $("#data").val("");
+            $("#horario").val("");
+            $("#profissional").val("");
+            $("#descricao").val("");
+            $("#tempo").val("");
+        }
+    }
+
+    /* exemplo
+    $.ajax({
+        url: "http://localhost:3000/pessoas",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            console.log(data); // Aqui você pode manipular os dados recebidos
+        },
+        error: function (error) {
+            console.log("Ocorreu um erro:", error);
+        },
+    });*/
 });
--- src/components/tarefas-ativas/tarefasAtivas.js
+++ src/components/tarefas-ativas/tarefasAtivas.js
@@ -1,25 +1,69 @@
 $(document).ready(function () {
     console.log("tarefas-ativas");
 
-    // TESTE - VERIFICANDO SE É POSSÍVEL MUDAR A EXIBIÇÃO DO COMPONENTE A PARTIR DE UM
-    // COMPONENTE FILHO - PASSOU NO TESTE
-    /*$(".btn-criar-tarefa").click(function () {
-        exibirCriarTarefa();
+    $(".enviar-tarefas-ativas").click(function () {
+        sendForward();
     });
 
-    function exibirCriarTarefa() {
-        $.ajax({
-            url: "../criar-tarefa/criarTarefa.js",
-            dataType: "script",
-            success: function (response) {},
+    $(".edit-button").click(function (e) {
+        editActivity(e);
+    });
+
+    $(".forward-button").click(function (e) {
+        openDialog(e);
+    });
+
+    function sendForward() {
+        var profissional = $("#forward-profissional").val();
+        var data = $("#forward-data").val();
+        var horario = $("#forward-horario").val();
+
+        $("#dialog-overlay").fadeOut();
+        $("#dialog-box").fadeOut();
+
+        $("#activity-table tbody tr").each(function () {
+            var row = $(this);
+            var cells = row.children("td");
+
+            if (cells.eq(2).text() === profissional && cells.eq(0).text() === data && cells.eq(1).text() === horario) {
+                row.remove();
+                return false;
+            }
         });
-        $.ajax({
-            url: "../criar-tarefa/criarTarefa.html",
-            dataType: "html",
-            success: function (response) {
-                $("#container-principal").html();
-                $("#container-principal").html(response);
-            },
-        });
-    }*/
+    }
+
+    function editActivity(button) {
+        var row = $(button).closest("tr");
+        var cells = row.children("td");
+
+        var data = cells.eq(0).text();
+        var horario = cells.eq(1).text();
+        var profissional = cells.eq(2).text();
+        var descricao = cells.eq(3).text();
+        var tempo = cells.eq(4).text();
+
+        $("#data").val(data);
+        $("#horario").val(horario);
+        $("#profissional").val(profissional);
+        $("#descricao").val(descricao);
+        $("#tempo").val(tempo);
+
+        row.remove();
+    }
+
+    function openDialog(button) {
+        var row = $(button).closest("tr");
+        var cells = row.children("td");
+
+        var profissional = cells.eq(2).text();
+        var data = cells.eq(0).text();
+        var horario = cells.eq(1).text();
+
+        $("#forward-profissional").val(profissional);
+        $("#forward-data").val(data);
+        $("#forward-horario").val(horario);
+
+        $("#dialog-overlay").fadeIn();
+        $("#dialog-box").fadeIn();
+    }
 });
